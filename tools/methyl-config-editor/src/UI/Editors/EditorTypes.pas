6b60f9d859fb02109685e360463763cb011c8251
David Izada Rodriguez <dizada@epimethyl.com>
2026-06-02 21:24:53 +0000

    Refactor IPropertyEditorContext interface by moving its definition to EditorTypes.pas
    The IPropertyEditorContext interface has been relocated from PropertyEditorContext.pas to EditorTypes.pas, enhancing modularity and organization of the code. This change streamlines the interface's accessibility and maintains consistency within the editor's architecture.

--- tools/methyl-config-editor/src/UI/Editors/EditorTypes.pas
+++ tools/methyl-config-editor/src/UI/Editors/EditorTypes.pas
@@ -11,8 +11,18 @@ uses
   SchemaNode;
 
 type
-  IPropertyEditorContext = interface;
   TPropertyRow = class;
+
+  IPropertyEditorContext = interface
+    ['{A1B2C3D4-1234-5678-90AB-CDEF12345678}']
+    function GetOwner: TComponent;
+    function GetObjectSchema: TSchemaNode;
+    function GetBreadcrumb: string;
+    function GetPropertyValue(const AName: string): TJSONValue;
+    procedure SetPropertyValue(const AName: string; AValue: TJSONValue);
+    procedure RebuildRows;
+    function ChildBreadcrumb(const ASegment: string): string;
+  end;
 
   ISchemaPropertyEditor = interface
     ['{E8A4B2C1-5D3F-4A2B-9C1E-7F6D5E4C3B2A}']
